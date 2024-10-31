de6c7b9185aaacdd6e192555490d63e0a2093144
LeH0712 <leanne_harrison7@hotmail.co.uk>
2024-10-31 17:43:56 +0000

    Added the AI API integration

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
+function displayRecipe(response) {
+  new Typewriter("#recipe", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 30,
+    cursor: null,
+  });
+}
+
 function generateRecipe(event) {
   event.preventDefault();
 
-  let recipeElement = document.querySelector("#recipe");
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "d0333349394fbbea5t3609f61c8982o3";
+  let prompt = `User instructions: Generate a cocktail recipe and instructions with ${instructionsInput.value}`;
+  let context =
+    "You are a cocktail recipe expert and have knowledge about every cocktail ingredient. Your mission is to generate a simple cocktail recipe in basic HTML and seperate each line. Do not write the word HTMl in the output. Make sure to follow the user instructions. Only include the name of the recipe and the list of ingredients and the instructions at the bottom";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  new Typewriter("#recipe", {
-    strings: "Woo Woo",
-    autoStart: true,
-    delay: 50,
-    cursor: null,
-  });
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#recipe-generator-form");
